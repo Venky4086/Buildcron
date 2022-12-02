b44acbb94bb35f01753cceb7e7b112fb45d36d6a
anurag <anuraganalogit@gmail.com>
2022-12-02 10:05:18 +0530

    baseurichanged

--- src/app/globaldata/global.data.ts
+++ src/app/globaldata/global.data.ts
@@ -1,6 +1,6 @@
 export class GlobalData {
-    public static  url_account:string = 'http://3.144.201.34:8000/api/account/';
+    public static  url_account:string = 'http://54.238.205.157:8000/api/account/';
     public static  url_buildcron:string = 'http://ec2-52-66-198-112.ap-south-1.compute.amazonaws.com/buildcron/api/';
-    public static  url_api:string = 'http://3.144.201.34:8000/api/';
+    public static  url_api:string = 'http://54.238.205.157:8000/api/';
     public static url_dashbaord:string = 'http://ec2-52-66-198-112.ap-south-1.compute.amazonaws.com/dashboard/api/';
   }
