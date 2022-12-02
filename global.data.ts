589fd50c27b6af9d36ea924d4418762137380335
anurag923 <71447965+anurag923@users.noreply.github.com>
2022-12-03 00:23:43 +0530

    delete gloable file

--- global.data.ts
+++
@@ -1,6 +0,0 @@
-export class GlobalData {
-    public static  url_account:string = 'http://18.183.4.74:8000/api/account/';
-    public static  url_buildcron:string = 'http://ec2-18-183-4-74.ap-northeast-1.compute.amazonaws.com/buildcron/api/';
-    public static  url_api:string = 'http://18.183.4.74:8000/api/';
-    public static url_dashbaord:string = 'http://ec2-18-183-4-74.ap-northeast-1.compute.amazonaws.com/dashboard/api/';
-  }
